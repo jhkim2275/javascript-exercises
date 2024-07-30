ce4b78ccb3df13ec1887d0e3108ddf95ef26d54f
Junghoon Kim <jhkim2275@gmail.com>
2024-07-30 20:04:57 +0900

    Reverse String
    I have coded the method so that the string gets reversed when passed through the given method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringArray = str.split("");
+    let ret = "";
+    for (let i = stringArray.length-1; i>=0; i--){
+        ret+=stringArray[i];
+    }
+    return ret;
 };
 
 // Do not edit below this line
