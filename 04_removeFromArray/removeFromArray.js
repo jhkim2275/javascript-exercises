445bb57070bbea13bb3206e94680da410a46d137
Junghoon Kim <jhkim2275@gmail.com>
2024-07-30 20:30:49 +0900

    Initial solution
    I have made a partly correct solution where it only takes one argument to be removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+    let index = -1;
+    for (let i = 0; i<arr.length; i++){
+        if (arr[i] == item){
+            index = i;
+        }
+    }
+    let ret = Array(arr.length-1);
+    let newIndex = 0;
+    for (let i = 0; i<arr.length; i++){
+        if (i != index) {
+            ret[newIndex] = arr[i];
+            newIndex++;
+        }
+    }
+    return ret;
 };
 
 // Do not edit below this line
