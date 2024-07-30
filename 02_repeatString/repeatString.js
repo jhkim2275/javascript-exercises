2a38ba0ea000669a01a1d3ccfd5c62ef3e0398d0
Junghoon Kim <jhkim2275@gmail.com>
2024-07-30 18:28:42 +0900

    Completed Code
    I have completed the method code for this project

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num<0) {return "ERROR";}
+    let ret = '';
+    for (let i = 0; i<num; i++) {
+        ret = ret + str;
+    }
+    return ret;
 };
 
 // Do not edit below this line
