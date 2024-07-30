fbf77d41af487c4cfac2559436076d8780b73f93
Junghoon Kim <jhkim2275@gmail.com>
2024-07-30 21:02:01 +0900

    Final
    I have passed all the test cases using the Gauses' sum technique

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != "number" || typeof end != "number") {return "ERROR";}
+    if (start%1 != 0 || end%1 != 0) {return "ERROR";}
+    if (start<0 || end <0) {return "ERROR";}
+    if (start>end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    sum = end+start;
+    return sum*(end-start+1)/2;
 };
 
 // Do not edit below this line
