df4ebba688fa6ae204d4321e7f74157911b8549f
Junghoon Kim <jhkim2275@gmail.com>
2024-07-30 20:32:57 +0900

    Final Solution
    Final solution after doing some research on array.filter() method and dealing with multiple arguments for a function in
    JavaScript

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,5 @@
-const removeFromArray = function(arr, item) {
-    let index = -1;
-    for (let i = 0; i<arr.length; i++){
-        if (arr[i] == item){
-            index = i;
-        }
-    }
-    let ret = Array(arr.length-1);
-    let newIndex = 0;
-    for (let i = 0; i<arr.length; i++){
-        if (i != index) {
-            ret[newIndex] = arr[i];
-            newIndex++;
-        }
-    }
-    return ret;
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(x => !args.includes(x));
 };
 
 // Do not edit below this line
