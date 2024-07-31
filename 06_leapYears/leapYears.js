d30c711c7e71793ea414c3f7de88c1d4fc09eaad
Junghoon Kim <jhkim2275@gmail.com>
2024-07-31 20:31:27 +0900

    Final Method
    I have coded the necessary method to complete the task

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let ans = false;
+    if (year%4 ==0) {ans = true;}
+    if (year%100 == 0) {ans = false;}
+    if (year%400 == 0) {ans = true;}
+    return ans;
 };
 
 // Do not edit below this line
