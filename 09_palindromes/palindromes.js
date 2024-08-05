3180aa3058e781961f287d37753b408b30417fe0
Junghoon Kim <jhkim2275@gmail.com>
2024-08-05 20:55:54 +0900

    Palindrome Complete
    I have made the code be able to detect any palindrome in a sentence

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const allowedString = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const trimmedString = str.toLowerCase().split("").filter((character) => allowedString.includes(character)).join("");
+    const reversedString = trimmedString.split('').reverse().join('');
+    return trimmedString === reversedString;
 };
 
 // Do not edit below this line
